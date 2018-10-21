e560b43cefbf09a66d12cfc918d48ac9250c74d0
Neil Patel <npatel023@gmail.com>
2018-10-21 12:43:57 -0500

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+function showHoursPopup(){
+    let element = document.getElementById('hours');
+    element.classList.toggle("show");
+}
+
+function closeHoursPopup(){
+    let element = document.getElementById('hours');
+    element.classList.toggle("show");
+}
